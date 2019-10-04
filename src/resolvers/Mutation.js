c48096351bfac21d6993f8c6a77fe87ea683d3ae
Eduar Apaza <educartoons@gmail.com>
2019-10-04 09:42:14 -0500

    Adding Delete Item function to our API

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,29 @@
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  }
+};
+
+module.exports = Mutations;
