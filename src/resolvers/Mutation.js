db0409b8880cddb5417e277ee450e01c456f1836
Eduar Apaza <educartoons@gmail.com>
2019-10-16 11:59:12 -0500

    Added Sign out function

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -83,6 +83,10 @@ const Mutations = {
       maxAge: 1000 * 60 * 60 * 24 * 365
     });
     return user;
+  },
+  signout(parent, args, context, info) {
+    context.response.clearCookie("token");
+    return { message: "Goodbye!" };
   }
 };
 
